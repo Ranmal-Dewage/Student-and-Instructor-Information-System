750b025262621a88958a47847b8955405a9f76c6
Ranmal-Dewage <ranmal.b.dewage@gmail.com>
2019-06-21 21:05:04 +0530

    edited 2 faculties degrees courses api with JWT

--- services/node/controller/facultyController.js
+++ services/node/controller/facultyController.js
@@ -45,28 +45,21 @@ route.post("/", tf.verifyToken, (req, res) => {
 })
 
 //get all faculties
-route.get("/", tf.verifyToken, (req, res) => {
-    
-    jwt.verify(req.token, config.secret, (err, authData) => {
-        if (err) {
-            console.log(err)
-            res.status(403).json({ status: "forbidden" })
-        } else {
-            try {
-                const query = {}
-                Faculties.find(query, (err, faculty) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).json({ error: err });
-                    } else {
-                        res.status(200).json({ faculties: faculty })
-                    }
-                });
-            } catch (ex) {
-                res.status(500).send("Server Error" + ex)
+route.get("/", (req, res) => {
+
+    try {
+        const query = {}
+        Faculties.find(query, (err, faculty) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ error: err });
+            } else {
+                res.status(200).json({ faculties: faculty })
             }
-        }
-    })
+        });
+    } catch (ex) {
+        res.status(500).send("Server Error" + ex)
+    }
 
 })
 
@@ -124,25 +117,19 @@ route.delete("/:code", tf.verifyToken, (req, res) => {
 //eg :- localhost:4000/faculties/IT/degrees
 route.get("/:code/degrees", tf.verifyToken, (req, res) => {
 
-    jwt.verify(req.token, config.secret, (err, authData) => {
-        if (err) {
-            res.status(403).json({ status: "forbidden" })
-        } else {
-            try {
-                const query = { fcode: req.params.code }
-                Degrees.find(query, (err, degree) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).json({ error: err });
-                    } else {
-                        res.status(200).json({ degrees: degree })
-                    }
-                });
-            } catch (ex) {
-                res.status(500).send("Server Error" + ex)
+    try {
+        const query = { fcode: req.params.code }
+        Degrees.find(query, (err, degree) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ error: err });
+            } else {
+                res.status(200).json({ degrees: degree })
             }
-        }
-    })
+        });
+    } catch (ex) {
+        res.status(500).send("Server Error" + ex)
+    }
 
 })
 

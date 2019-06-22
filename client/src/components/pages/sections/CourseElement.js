ae4bcebe0ffd009442795f382e857045caffbecc
tenusha <tenushamadhushan@gmail.com>
2019-06-22 13:14:29 +0530

    enhansements

--- client/src/components/pages/sections/CourseElement.js
+++ client/src/components/pages/sections/CourseElement.js
@@ -1,12 +1,13 @@
 import {MDBCol, MDBCard, MDBCardBody, MDBCardText, MDBBtn, MDBInput, MDBFormInline} from "mdbreact"
 import React, {Component} from "react"
 import {updateUser} from '../../functions/Services'
-import {toast} from "react-toastify";
+import {toast} from "react-toastify"
 
 export default class CourseElement extends Component {
 
     state = {
-        ek: ''
+        ek: '',
+        user: {}
     }
 
     handleChange = event => {
@@ -17,31 +18,42 @@ export default class CourseElement extends Component {
         this.setState({[event.target.name]: value})
     }
 
+    componentDidMount() {
+        var user = localStorage.getItem('sis-user')
+        if (user) {
+            user = JSON.parse(user)
+            this.setState({user})
+        }
+    }
+
     handleSubmit = event => {
-        if (this.state.ek === this.props.course.ccode) {
-            var user = localStorage.getItem('sis-user')
-            if (user) {
-                user = JSON.parse(user)
+        if (this.state.user && this.state.user.permissionLevel === 1) {
+            if (this.state.ek === this.props.course.ccode) {
+                const body = {
+                    id: this.state.user.id,
+                    courses: this.state.user.courses ? [...this.state.user.courses, this.props.course.ccode] : [this.props.course.ccode]
+                }
+                updateUser(body)
+                    .then(res => {
+                        const data = {...this.state.user, courses: body.courses}
+                        localStorage.setItem('sis-user', JSON.stringify(data))
+                        window.location = "/courses/" + this.props.course.ccode + "/view"
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+            } else {
+                toast.error("Invalid enrollment key!!!")
             }
-            const body = {
-                id: user.id,
-                courses: user.courses ? [...user.courses, this.props.course.ccode] : [this.props.course.ccode]
-            }
-            updateUser(body)
-                .then(res => {
-                    console.log(res)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
         } else {
-            toast.error("Invalid enrollment key!!!")
+            toast.error("Only students can enroll to this course!!!")
         }
         event.preventDefault()
         event.stopPropagation()
     }
 
     render() {
+        const link = "/courses/" + this.props.course.ccode + "/view"
         return (
             <MDBCol className="my-3">
                 <MDBCard>
@@ -49,18 +61,24 @@ export default class CourseElement extends Component {
                         <MDBCardText>
                             {this.props.course.description}
                         </MDBCardText>
-                        <MDBFormInline onSubmit={this.handleSubmit}>
-                            <MDBInput
-                                label="Type enrollment key"
-                                group
-                                name="ek"
-                                type="password"
-                                validate
-                                onChange={this.handleChange}
-                                required
-                            />
-                            <MDBBtn size="sm" type="submit">Enroll</MDBBtn>
-                        </MDBFormInline>
+                        {this.state.user && this.state.user.courses && this.state.user.courses.includes(this.props.course.ccode) ?
+                            <p onClick={() => window.location = link} className="text-primary">You have already enrolled
+                                to
+                                this course</p>
+                            :
+                            <MDBFormInline onSubmit={this.handleSubmit}>
+                                <MDBInput
+                                    label="Type enrollment key"
+                                    group
+                                    name="ek"
+                                    type="password"
+                                    validate
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                                <MDBBtn size="sm" type="submit">Enroll</MDBBtn>
+                            </MDBFormInline>
+                        }
                     </MDBCardBody>
                 </MDBCard>
             </MDBCol>
